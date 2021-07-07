3ee85c2187c122f56dec2366760353ec571884df
m2cci-NMZ <nico100885@hotmail.Com>
2021-07-07 17:48:24 +0200

    fixed shading when triangles when triangles where not facing light

--- Shader.cpp
+++ Shader.cpp
@@ -1,20 +1,31 @@
 #include "Shader.h"
 
-Shader::Shader(){
+Shader::Shader()
+{
     lightDir.setZ(-1.0);
     lightDir.setY(1.0);
 }
-void Shader::setLightDir(Point p){
+void Shader::setLightDir(Point p)
+{
     lightDir = p;
 }
-TriMesh Shader::computeShader(TriMesh object){
+TriMesh Shader::computeShader(TriMesh object)
+{
     TriMesh out;
     lightDir.normalize();
-    for (auto tri: object.getTriangles()){
-            tri.computeNormal();
-            float lum = lightDir.dotProduct(tri.getNormal());
+    for (auto tri : object.getTriangles())
+    {
+        tri.computeNormal();
+        float lum = lightDir.dotProduct(tri.getNormal());
+        if (lum > 0)
+        {
             tri.setLum(lum);
-            out.addTriangle(tri);
+        }
+        else
+        {
+            tri.setLum(0.0);
+        }
+        out.addTriangle(tri);
     }
     return out;
 }
