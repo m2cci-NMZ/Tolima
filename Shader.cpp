df7e3b4531ee664c74b60ad271c2d0491d26130f
m2cci-NMZ <nico100885@hotmail.Com>
2021-10-28 10:55:45 +0200

    Added phong illumination model tu the Shader classs

--- Shader.cpp
+++ Shader.cpp
@@ -2,7 +2,7 @@
 
 Shader::Shader()
 {
-    lightDir.setZ(-1.0);
+    lightDir.setZ(10.0);
     lightDir.setY(1.0);
 }
 void Shader::setLightDir(Point p)
@@ -10,34 +10,98 @@ void Shader::setLightDir(Point p)
     lightDir = p;
 }
 
-void Shader::computeVertIntensities(Triangle &t)
+void Shader::computeVertIntensities(Triangle &t, Point camera_pos)
 {
-    lightDir.normalize();
-    intensityA = t.getNA().dotProduct(lightDir);
-    intensityB = t.getNB().dotProduct(lightDir);
-    intensityC = t.getNC().dotProduct(lightDir);
-    if (intensityA < 0)
+    // light direction vectors
+    Point La, Lb, Lc;
+    La = this->lightDir - t.getA();
+    Lb = this->lightDir - t.getB();
+    Lc = this->lightDir - t.getC();
+    La.normalize();
+    Lb.normalize();
+    Lc.normalize();
+    // View direction vecors
+    Point Va, Vb, Vc;
+    Va = camera_pos - t.getA();
+    Vb = camera_pos - t.getB();
+    Vc = camera_pos - t.getC();
+    Va.normalize();
+    Vb.normalize();
+    Vc.normalize();
+    // Reflection direction vector
+    Point Ra, Rb, Rc;
+    Ra = t.getNA() * (2 * (La.dotProduct(t.getNA()))) - La;
+    Rb = t.getNB() * (2 * (Lb.dotProduct(t.getNB()))) - Lb;
+    Rc = t.getNC() * (2 * (Lc.dotProduct(t.getNC()))) - Lc;
+    Ra.normalize();
+    Rb.normalize();
+    Rc.normalize();
+
+    // Compute lighting by looping over RGB
+    for (int i = 0; i < 3; i++)
     {
-        intensityA = 0;
-    }
-    if (intensityB < 0)
-    {
-        intensityB = 0;
-    }
-    if (intensityC < 0)
-    {
-        intensityC = 0;
+        intensityA[i]=Ka[i] + max(Kd[i]*(La.dotProduct(t.getNA())),0)+ max(Ks[i]*pow(Ra.dotProduct(Va),_Ns),0);
+        intensityB[i]=Ka[i] + max(Kd[i]*(Lb.dotProduct(t.getNB())),0)+ max(Ks[i]*pow(Rb.dotProduct(Vb),_Ns),0);
+        intensityC[i]=Ka[i] + max(Kd[i]*(Lc.dotProduct(t.getNC())),0)+ max(Ks[i]*pow(Rc.dotProduct(Vc),_Ns),0);
     }
 }
-float Shader::getIntensityA()
+
+float Shader::getIntensityAR()
 {
-    return intensityA;
+    return intensityA[0];
 }
-float Shader::getIntensityB()
+float Shader::getIntensityAG()
 {
-    return intensityB;
+    return intensityA[1];
 }
-float Shader::getIntensityC()
+float Shader::getIntensityAB()
 {
-    return intensityC;
+    return intensityA[2];
+}
+
+float Shader::getIntensityBR()
+{
+    return intensityB[0];
+}
+float Shader::getIntensityBG()
+{
+    return intensityB[1];
+}
+float Shader::getIntensityBB()
+{
+    return intensityB[2];
+}
+
+float Shader::getIntensityCR()
+{
+    return intensityC[0];
+}
+float Shader::getIntensityCG()
+{
+    return intensityC[1];
+}
+float Shader::getIntensityCB()
+{
+    return intensityC[2];
+}
+
+void Shader::setKa(float r, float g, float b)
+{
+    this->Ka[0] = r;
+    this->Ka[1] = g;
+    this->Ka[2] = b;
+}
+
+void Shader::setKd(float r, float g, float b)
+{
+    this->Kd[0] = r;
+    this->Kd[1] = g;
+    this->Kd[2] = b;
+}
+
+void Shader::setKs(float r, float g, float b)
+{
+    this->Ks[0] = r;
+    this->Ks[1] = g;
+    this->Ks[2] = b;
 }
--- Shader.h
+++ Shader.h
@@ -2,19 +2,36 @@
 #include "Point.h"
 #include "Triangle.h"
 #include "TriMesh.h"
+#include <tgmath.h> 
 
 class Shader{
     private:
         Point lightDir;
-        float intensityA;
-        float intensityB;
-        float intensityC;
+        float intensityA[3];
+        float intensityB[3];
+        float intensityC[3];
+        float _Ns; 
+        float Ka[3];
+        float Kd[3];
+        float Ks[3];
+
+
 
     public:
         Shader();
         void setLightDir(Point p);
-        void computeVertIntensities(Triangle& t);
-        float getIntensityA();
-        float getIntensityB();
-        float getIntensityC();
+        void computeVertIntensities(Triangle& t, Point camera_pos);
+        float getIntensityAR();
+        float getIntensityAG();
+        float getIntensityAB();
+        float getIntensityBR();
+        float getIntensityBG();
+        float getIntensityBB();
+        float getIntensityCR();
+        float getIntensityCG();
+        float getIntensityCB();
+        void setKa(float r, float g, float b);
+        void setKd(float r, float g, float b);
+        void setKs(float r, float g, float b);
+        void setNs(float Ns);
 };
